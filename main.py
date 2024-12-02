c0fce31b70f1a2b746b6f0f7ec44c2d10dd28142
Ngọc Lam <nglamvn2911@gmail.com>
2024-12-02 14:00:39 +0700

    Remove rtmp module

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from api import APIv1
 from app import app as webapp
-from rtmp.rtmp import loader
-from command import command_thread, command_handler
-import asyncio
-import threading
 import dotenv
 import os
 from pathlib import Path
@@ -20,32 +16,13 @@ app = Flask(__name__)
 host = os.getenv("HOST")
 port = os.getenv("PORT")
 
-def api_thread():
+def api_init():
     app.register_blueprint(APIv1)
     app.register_blueprint(webapp)
     app.wsgi_app = ProxyFix(
         app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
     )
     app.run(debug=True, host=host, port=port, use_reloader=False)
-    
-def rtmp_thread():
-    asyncio.run(loader())
 
 if __name__ == '__main__':
-    api = threading.Thread(target=api_thread)
-    rtmp = threading.Thread(target=rtmp_thread)
-    cmd = threading.Thread(target=command_thread)
-    handler = threading.Thread(target=command_handler)
-    
-    api.start()
-    rtmp.start()
-    cmd.start()
-    handler.start()
-    
-    try:
-        api.join()
-        rtmp.join()
-        cmd.join()
-        handler.join()
-    except KeyboardInterrupt:
-        print("Shutting down gracefully...")
+    api_init()
