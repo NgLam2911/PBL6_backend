6d14cc5bdcd6d9bd3afc4e62b9518a00f4a0cd25
Ngọc Lam <nglamvn2911@gmail.com>
2024-12-10 13:05:59 +0700

    Redo most of things just a proxy bug...

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, send_file, request, Response
+from flask import Flask, Blueprint, send_file
 import os
-import re
 from http import HTTPStatus
 from dotenv import load_dotenv
 from _utils import createThumbnail
@@ -11,42 +10,12 @@ app = Blueprint('app', __name__, url_prefix='/')
 def get_video(uuid):
     load_dotenv()
     path = os.getenv('VIDEO_SAVE_PATH')
-    file_name = f'{uuid}.mp4'
-    file_path = os.path.join(path, file_name)
-    
-    if not os.path.exists(file_path):
+    file_name = os.path.join(path, f'{uuid}.mp4')
+    if not os.path.exists(file_name):
         return 'Video not found', HTTPStatus.NOT_FOUND
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(file_name):
         return 'Invalid video', HTTPStatus.BAD_REQUEST
-    
-    range_header = request.headers.get('Range', None)
-    if not range_header:
-        return send_file(file_path, mimetype='video/mp4')
-    
-    size = os.path.getsize(file_path)
-    byte1, byte2 = 0, None
-
-    if range_header:
-        match = re.search(r'(\d+)-(\d*)', range_header)
-        if match:
-            byte1, byte2 = match.groups()
-            byte1 = int(byte1)
-            if byte2:
-                byte2 = int(byte2)
-    
-    length = size - byte1
-    if byte2 is not None:
-        length = byte2 - byte1 + 1
-    
-    with open(file_path, 'rb') as f:
-        f.seek(byte1)
-        data = f.read(length)
-    
-    rv = Response(data, 206, mimetype='video/mp4', content_type='video/mp4', direct_passthrough=True)
-    rv.headers.add('Content-Range', f'bytes {byte1}-{byte1 + length - 1}/{size}')
-    rv.headers.add('Accept-Ranges', 'bytes')
-    
-    return rv
+    return send_file(file_name, mimetype='video/mp4'), HTTPStatus.OK
 
 @app.route('/thumbnail/<string:uuid>', methods=['GET'])
 def get_thumbnail(uuid):
